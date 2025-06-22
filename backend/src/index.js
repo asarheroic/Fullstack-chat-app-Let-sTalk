bdbede482ddea506aace504ef3151ba6ac103f4d
asarheroic <130447250+asarheroic@users.noreply.github.com>
2025-06-23 03:28:51 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -33,14 +33,6 @@ app.use("/api/auth" ,authRoutes)
 app.use("/api/message" ,messageRoutes)
 
 
-if(process.env.NODE_ENV=='production'){
-  app.use(express.static(path.join(__dirname , '../frontend/dist')))
-
-  app.get('*', (req,res) => {
-    res.sendFile(path.join(__dirname ,'../frontend','dist',"index.html"))
-  }
-  )
-}
 
 
 
