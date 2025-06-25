fd88a4a035bde919557c78b13d2cfb5255ab1ba1
asarheroic <130447250+asarheroic@users.noreply.github.com>
2025-06-26 02:53:58 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const axiosinstance =  axios.create({
-    baseURL : import.meta.env.VITE_MODE=== "development" ? `${import.meta.env.VITE_BASE_URL}/api`: "/api",
+    baseURL :  `${import.meta.env.VITE_BASE_URL}/api`,
     withCredentials : true
 })
